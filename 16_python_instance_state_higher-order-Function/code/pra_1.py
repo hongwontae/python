3503cb8aecbf3a9f8c155acc4ea3f9c466412372
dnjsxoghd <feelchok1234@gmail.com>
2024-11-15 15:55:18 +0900

    class object higher function

---
+++ 16_python_instance_state_higher-order-Function/code/pra_1.py
@@ -0,0 +1,19 @@
+from turtle import Turtle, Screen;
+import random
+
+screen = Screen()
+
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title='Make Your best',prompt='which turtle will win the race? Enter a color')
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
+y_positions = [-70, -40, -10, 20, 50, 80]
+
+for turtle_index in range(0,6) :
+    tim = Turtle(shape='turtle')
+    tim.penup()
+    tim.color(colors[turtle_index])
+    tim.goto(x=-230, y=-y_positions[turtle_index])
+    
+
+
+screen.exitonclick()
