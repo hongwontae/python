1c03bc08162bc2494f07ff27c3aaec495da98c76
dnjsxoghd <feelchok1234@gmail.com>
2024-11-20 14:44:38 +0900

    list, dictionary, data_freame comprehension

---
+++ 22_py_list_comprehension_dictionary_compre/code/d_c.py
@@ -0,0 +1,23 @@
+# import random
+
+# new_names = ['Alex', 'Bath', 'Caroline', 'Dave', 'Eleanor', 'Freddie']
+
+# student_scores = {student : random.randint(1,100) for student in new_names}
+# print(student_scores)
+
+# pass_students = {student:score for (student, score) in student_scores.items() if score >= 60}
+# print(pass_students)
+
+# data_3 = pass_students.keys()
+# print(data_3)
+
+# sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+# sentence_word = sentence.split(' ')
+# result = {word : len(word) for word in sentence_word}
+# print(result)
+
+# studnet = {'sss' : 10, 'd2' : 20}
+# print(studnet.items())
+# for s in studnet :
+#     print(s)
+
---
+++ 22_py_list_comprehension_dictionary_compre/code/main.py
@@ -0,0 +1,30 @@
+import pandas;
+
+students_socrs = {
+    'student' : ['Alexander', 'Kiesa', 'Robertson', 'Graven-berch'],
+    'score' : [75, 30, 70, 90]
+}
+
+data_frame_student = pandas.DataFrame(students_socrs)
+data_list = []
+
+# series
+# print(data_frame_student.score)
+# # 일치하는 series 행을 찾습니다.
+# print(data_frame_student[data_frame_student.score == 30])
+for (index, row) in data_frame_student.iterrows() :
+    print(row.score)
+
+# for (key, value) in data_frame_student.items() :
+#     data_list.append(key)
+
+# print(data_list)
+# print('???')
+
+# for (index, row) in data_frame_student.iterrows() :
+#     print(row)
+#     data_list.append(row.student)
+# print(data_list)
+
+# print(data_frame_student.student[0])12
+
