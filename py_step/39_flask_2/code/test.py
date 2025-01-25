16ab00688537938b7bfa7504b1136ed8249f5158
dnjsxoghd <feelchok1234@gmail.com>
2025-01-25 10:45:18 +0900

    flask deco

--- py_step/39_flask_2/code/test.py
+++ py_step/39_flask_2/code/test.py
@@ -1,9 +1,20 @@
-from flask import Flask;
+class User :
+    def __init__(self, name):
+        self.name = name
+        self.is_logged_in = False
 
-app = Flask(__name__)
+
+def is_authentication (function) :
+    def wrapper_function (*args) :
+        if args[0].is_logged_in == True :
+            function(args[0])
+    return wrapper_function
 
 
+@is_authentication
+def create_blog (user) :
+    print(f'This is {user.name} new blog post')
 
-
-if __name__ == '__main__' :
-    app.run()
+new_user = User('HWT')
+new_user.is_logged_in = True
+create_blog(new_user)
