60396a178d16b6726547b6d1b826ea2836caf92c
dnjsxoghd <feelchok1234@gmail.com>
2026-03-25 16:07:03 +0900

    update

--- py_AI/model/multiple-linear-regression/main.py
+++ py_AI/model/multiple-linear-regression/main.py
@@ -3,6 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 
 df = pd.read_csv('50_Startups.csv')
 x = df.iloc[:,:-1].values
@@ -11,5 +13,18 @@ y = df.iloc[:, -1].values
 # 3개의 카테고리를 one-hot-encoder으로 변환완료
 ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [3])], remainder='passthrough')
 x = np.array(ct.fit_transform(x))
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+
+# 다중 선형 회귀 모델 구축
+regressor = LinearRegression()
+regressor.fit(x_train, y_train)
+
+# 예측값
+y_pred = regressor.predict(x_test)
+np.set_printoptions(precision=2)
+print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test),1)),1))
+
+
 
 
