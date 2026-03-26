fec76508a2dd2563734306510b64d6832b699a77
dnjsxoghd <feelchok1234@gmail.com>
2026-03-26 16:05:48 +0900

    multiple regressor understand

--- py_AI/model/multiple-linear-regression/main.py
+++ py_AI/model/multiple-linear-regression/main.py
@@ -11,7 +11,7 @@ x = df.iloc[:,:-1].values
 y = df.iloc[:, -1].values
 
 # 3개의 카테고리를 one-hot-encoder으로 변환완료
-ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [3])], remainder='passthrough')
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(drop='first'), [3])], remainder='passthrough')
 x = np.array(ct.fit_transform(x))
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
@@ -20,7 +20,8 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_
 regressor = LinearRegression()
 regressor.fit(x_train, y_train)
 
-# 예측값
+
+# 예측값 ###
 y_pred = regressor.predict(x_test)
 np.set_printoptions(precision=2)
 print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test),1)),1))
