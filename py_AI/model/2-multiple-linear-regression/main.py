3a55c75d5e9787d8a8386dc52e7ba286fb259de9
dnjsxoghd <feelchok1234@gmail.com>
2026-03-30 16:54:39 +0900

    update

--- py_AI/model/2-multiple-linear-regression/main.py
+++ py_AI/model/2-multiple-linear-regression/main.py
@@ -15,6 +15,7 @@ ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(drop='first'), [3
 x = np.array(ct.fit_transform(x))
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+print(x_test)
 
 # 다중 선형 회귀 모델 구축
 regressor = LinearRegression()
@@ -24,7 +25,7 @@ regressor.fit(x_train, y_train)
 # 예측값 ###
 y_pred = regressor.predict(x_test)
 np.set_printoptions(precision=2)
-print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test),1)),1))
+# print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test),1)),1))
 
 
 
--- py_AI/model/3-polynomial-linear-regressor/main.py
+++ py_AI/model/3-polynomial-linear-regressor/main.py
@@ -12,10 +12,29 @@ x = dataset.iloc[:, 1:-1].values
 # 백터
 y = dataset.iloc[:, -1].values
 
+
 # 단순 선형 회귀 모델 구축
-simple_regressor = LinearRegression()
-simple_regressor.fit(x,y)
+lin_reg = LinearRegression()
+lin_reg.fit(x,y)
 
 # 다항식 선형 회귀를 위한 전처리
+poly_reg = PolynomialFeatures(degree=2)
+x_poly = poly_reg.fit_transform(x)
+lin_reg2 = LinearRegression()
+lin_reg2.fit(x_poly, y)
 
+# graph Simple Linear Regressor
+# plt.scatter(x,y, color='red')
+# plt.plot(x, lin_reg.predict(x), color='blue')
+# plt.title('Truth or Bluff (Linear Regressor)')
+# plt.xlabel('Position Level')
+# plt.ylabel('Salary')
+# plt.show()
 
+# Graph Poly Linear Regressor
+plt.scatter(x,y, color='red')
+plt.plot(x, lin_reg2.predict(poly_reg.fit_transform(x)), color='blue')
+plt.title('Truth or Bluff (Poly Regressor)')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show()
